307bede150788d10c185418263b3c5bc5845e7b9
Lazar Dobric <morphin3521@gmail.com>
2026-07-05 00:34:36 +0200

    file formatting; readme goals updated

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Mixed-radix FFT software model and hardware implementation
 
-## Python:
-- FFT radix 2, 3, 5 (float)
-- Digit inversion permutation of inputs/outputs
-- FFT radix 2, 3, 5 (fixed-point)
+
+## Done items:
+- Python FFT radix 2, 3, 5 (float model)
+- Python Digit inversion permutation of inputs/outputs
+- Python FFT radix 2, 3, 5 (fixed-point model)
 - SQNR evaluation
+- Automatic FFT FXP model evaluation through different parameters of design
+
+- VHDL reconfigurable stage preadder with it's testbench
 
 ## To-do:
-- Test the performance
-- Hardware implementation (fixed and reconfigurable architectures)
+- VHDL reconfigurable rotator and twiddle factor gen/storage
+- VHDL stage FIFOs
+- VHDL stage control logic
+- VHDL MR stage
--- rtl/tb/tb_mr_fft_preadder.vhd
+++ rtl/tb/tb_mr_fft_preadder.vhd
@@ -10,49 +10,49 @@ entity tb_mr_fft_preadder is
 end entity tb_mr_fft_preadder;
 
 architecture tb of tb_mr_fft_preadder is
-    signal i_x0    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
-    signal i_x1    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
-    signal i_x2    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
-    signal i_x3    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
-    signal i_x4    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
-    
-    signal i_s0    : std_logic_vector(1 downto 0) := "00";
-    signal i_s1    : std_logic := '0';
+	signal i_x0    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
+	signal i_x1    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
+	signal i_x2    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
+	signal i_x3    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
+	signal i_x4    : t_cmplx := (re => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width), im => to_sfixed(0, c_fxp_int_width-1, -c_fxp_frac_width));
+	
+	signal i_s0    : std_logic_vector(1 downto 0) := "00";
+	signal i_s1    : std_logic := '0';
 
-    signal o_X0    : t_cmplx;
-    signal o_X1    : t_cmplx;
-    signal o_X2    : t_cmplx;
-    signal o_X3    : t_cmplx;
-    signal o_X4    : t_cmplx;
+	signal o_X0    : t_cmplx;
+	signal o_X1    : t_cmplx;
+	signal o_X2    : t_cmplx;
+	signal o_X3    : t_cmplx;
+	signal o_X4    : t_cmplx;
 begin
-    dut: entity work.mr_fft_preadder
-        port map (
-            i_x0 => i_x0,
-            i_x1 => i_x1,
-            i_x2 => i_x2,
-            i_x3 => i_x3,
-            i_x4 => i_x4,
-            i_s0 => i_s0,
-            i_s1 => i_s1,
-            o_X0 => o_X0,
-            o_X1 => o_X1,
-            o_X2 => o_X2,
-            o_X3 => o_X3,
-            o_X4 => o_X4
-        );
+	dut: entity work.mr_fft_preadder
+		port map (
+			i_x0 => i_x0,
+			i_x1 => i_x1,
+			i_x2 => i_x2,
+			i_x3 => i_x3,
+			i_x4 => i_x4,
+			i_s0 => i_s0,
+			i_s1 => i_s1,
+			o_X0 => o_X0,
+			o_X1 => o_X1,
+			o_X2 => o_X2,
+			o_X3 => o_X3,
+			o_X4 => o_X4
+		);
 
-    stimulus: process
-    begin
-        i_x0 <= (re => to_sfixed(1.0, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(1.0, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
-        i_x1 <= (re => to_sfixed(0.5, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(0.5, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
-        i_x2 <= (re => to_sfixed(0.3, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(0.3, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
-        i_x3 <= (re => to_sfixed(0.2, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(0.2, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
-        i_x4 <= (re => to_sfixed(0.1, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(0.1, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
-        i_s0 <= "10";
-        i_s1 <= '1';
-        wait for 10 ns;
-        wait for 10 ns;
+	stimulus: process
+	begin
+		i_x0 <= (re => to_sfixed(1.0, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(1.0, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
+		i_x1 <= (re => to_sfixed(0.5, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(0.5, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
+		i_x2 <= (re => to_sfixed(0.3, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(0.3, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
+		i_x3 <= (re => to_sfixed(0.2, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(0.2, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
+		i_x4 <= (re => to_sfixed(0.1, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate), im => to_sfixed(0.1, c_fxp_int_width-1, -c_fxp_frac_width, fixed_wrap, fixed_truncate));
+		i_s0 <= "10";
+		i_s1 <= '1';
+		wait for 10 ns;
+		wait for 10 ns;
 
-        wait;
-    end process stimulus;
+		wait;
+	end process stimulus;
 end architecture tb;
